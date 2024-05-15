bafc02c8156b2cfef294ecb734669fcc80f74afa
Mucahit <mchtsenn16@gmail.com>
2024-05-15 09:03:39 +0300

    commented

--- lib/services/user_repo/a_user_repository.dart
+++ lib/services/user_repo/a_user_repository.dart
@@ -1,9 +1,11 @@
 import 'package:quiz_app/utils/models/my_user_model.dart';
-
+/// User Rep 
 abstract class UserRepository {
   Stream<MyUser?> get user;
   Future<MyUser> signUp(MyUser myUser, String password);
   Future<void> setUserData(MyUser myUser);
   Future<void> signIn(String email, String password);
+
+  /// log out func description
   Future<void> logOut();
 }
